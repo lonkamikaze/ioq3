80eb1e54e7039f2a6f6cbcbcbd9e0778c1252839
Dominic Fandrey <kamikaze@bsdforen.de>
2016-11-26 15:41:22 +0100

    Use unsigned for sum to get defined overflow

--- code/qcommon/common.c
+++ code/qcommon/common.c
@@ -1378,7 +1378,7 @@ Touch all known used data to make sure it is paged in
 void Com_TouchMemory( void ) {
 	int		start, end;
 	int		i, j;
-	int		sum;
+	unsigned int	sum;
 	memblock_t	*block;
 
 	Z_CheckHeap();
@@ -1389,20 +1389,20 @@ void Com_TouchMemory( void ) {
 
 	j = hunk_low.permanent >> 2;
 	for ( i = 0 ; i < j ; i+=64 ) {			// only need to touch each page
-		sum += ((int *)s_hunkData)[i];
+		sum += ((unsigned int *)s_hunkData)[i];
 	}
 
 	i = ( s_hunkTotal - hunk_high.permanent ) >> 2;
 	j = hunk_high.permanent >> 2;
 	for (  ; i < j ; i+=64 ) {			// only need to touch each page
-		sum += ((int *)s_hunkData)[i];
+		sum += ((unsigned int *)s_hunkData)[i];
 	}
 
 	for (block = mainzone->blocklist.next ; ; block = block->next) {
 		if ( block->tag ) {
 			j = block->size >> 2;
 			for ( i = 0 ; i < j ; i+=64 ) {				// only need to touch each page
-				sum += ((int *)block)[i];
+				sum += ((unsigned int *)block)[i];
 			}
 		}
 		if ( block->next == &mainzone->blocklist ) {
